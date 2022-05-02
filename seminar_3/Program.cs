f53d9c822788b16a2a8f5778e7c186065f2198fa
Anna <ds_anna@bk.ru>
2022-05-02 20:40:25 +0300

    max

---
+++ seminar_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("input digit A ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("input digit B ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("input C ");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write("max= ");
+Console.WriteLine(max);
