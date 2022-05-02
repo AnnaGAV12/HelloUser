558cc5dfcb0cc8bfa423e1058c1d09c0f92cfd2a
Anna <ds_anna@bk.ru>
2022-05-02 21:36:05 +0300

    док-во,что число трехзначное

---
+++ seminar_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите 3х-значное число ");
+int a = int.Parse(Console.ReadLine());
+if (a > 99 && a < 1000)
+{
+    Console.Write("Последняя цифра ");
+    Console.WriteLine(a % 10);
+}
+else
+{
+    Console.Write("Число не 3х-значное");
+}
