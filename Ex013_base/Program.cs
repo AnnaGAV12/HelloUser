50af789c332bfb4df8a59adf3d895c1c74de7828
Anna <ds_anna@bk.ru>
2022-05-02 17:49:03 +0300

    Ex013_base

---
+++ Ex013_base/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 3;
+int b = 6;
+int c = 9;
+int d = 2;
+int e = 1;
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("max= ");
+Console.WriteLine(max);
