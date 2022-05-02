c1662ea183fb9a8204e59726c5b13188bdb161ee
Anna <ds_anna@bk.ru>
2022-05-02 22:40:06 +0300

    Name

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Введите ваше имя");
-string username = Console.ReadLine();
-Console.Write ("Привет");
-Console.Write (username);
+﻿Console.Write("What is your name?");
+string name = Console.ReadLine();
+Console.Write("How old are you?");
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine(" Your name is" + name + ". Your are" + age + "years old.");
